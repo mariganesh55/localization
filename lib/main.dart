004eb252111c67759e2dfee976a9fb6640a9f743
mariganesh55 <mariganeshthangam@gmail.com>
2022-09-28 00:01:32 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:get/get.dart';
+import 'package:localization/controller.dart';
+
+import 'app_localizations.dart';
+import 'homepage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    Get.put(LocaleCont());
+    return GetBuilder<LocaleCont>(
+      builder: (cont) => MaterialApp(
+        locale: cont.locale,
+        debugShowCheckedModeBanner: false,
+        localeResolutionCallback: (deviceLocale, supportedLocales) {
+          for (var locale in supportedLocales) {
+            if (locale.languageCode == deviceLocale!.languageCode &&
+                locale.countryCode == deviceLocale.countryCode) {
+              return deviceLocale;
+            }
+          }
+          return supportedLocales.first;
+        },
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        localizationsDelegates: const [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+          AppLocalization.delegate,
+        ],
+        supportedLocales: const [
+          Locale('en', 'US'),
+          Locale('ne', 'NP'),
+          Locale('es', 'AR'),
+        ],
+        home: const HomePage(),
+      ),
+    );
+  }
+}
